16392f6c8e0b7f80f31ee65c3dfef6f3220e18d3
Yuri <yuri.kuniver@gmail.com>
2022-08-25 16:00:49 +0300

    So what.ssssssssssssssss..

--- hello.py
+++ hello.py
@@ -4,16 +4,14 @@ print("Fixing some bug...")
 
 print("While someone fixes the bug, we are moving forward...")
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 print("Okay, really fixing some bug... or not?")
 
 print("Okay, guys...")
 
 print("So what..")
-=======
+
 print("Okay, really fixing some bug... or not?")
->>>>>>> parent of fd27031 (Guys...)
-=======
+
 print("Okay, really fixing some bug... or not?")
->>>>>>> parent of fd27031 (Guys...)
+
+
